9b3f27a68ea3946bdd6d36bfc7ba241b5d7a0738
Lennart Svanberg <lennartsvanberg@gmail.com>
2024-05-31 10:16:59 +0200

    Lagt till val för soffor

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-// You can add JavaScript here if needed for interactivity or functionality
+function updateSofaOptions() {
+    const sofaSelect = document.getElementById('sofa-select');
+    const sofaModelSelect = document.getElementById('sofa-model-select');
+    
+    const options = {
+        dux: ["Dux Fredrik", "Dux Johan"],
+        eilersen: ["Eilersen Ash", "Eilersen Lift"],
+        fogia: ["Fogia Duni", "Fogia Morris"],
+        "annan-soffa": ["Jag har en 3-sits soffa", "Jag har en 2-sits soffa"]
+    };
+
+    const selectedSofa = sofaSelect.value;
+
+    if (selectedSofa && options[selectedSofa]) {
+        sofaModelSelect.innerHTML = "";
+        options[selectedSofa].forEach(model => {
+            const option = document.createElement('option');
+            option.value = model.toLowerCase().replace(/ /g, '-');
+            option.textContent = model;
+            sofaModelSelect.appendChild(option);
+        });
+        sofaModelSelect.style.display = "block";
+    } else {
+        sofaModelSelect.style.display = "none";
+    }
+}
